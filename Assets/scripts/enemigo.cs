4879d2d3492b31d487dd7793c1913bcd1ec0ffcc
lasoler <solerdiazcristina9@gmail.com>
2026-04-17 13:11:20 +0200

    enemigo moverse

--- Assets/scripts/enemigo.cs
+++ Assets/scripts/enemigo.cs
@@ -1,17 +1,49 @@
+using System;
 using UnityEngine;
 
 public class enemigo : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    //estado de enemigo: patrulla, deteccion, persecucion
+    String estado = "patrulla";
+
+    public float distanciaPatrulla = 2.0f;
+
+    public float velocidadPatrulla = 0.02f;
+
+    Vector3 posicionInicial;
+    Vector3 posicionLimitIzq, posicionLimitDer;
+
+
     void Start()
     {
-        
+        posicionInicial = transform.position;
+        posicionLimitIzq = new Vector3(posicionInicial.x - distanciaPatrulla, posicionInicial.y, 0);
+        posicionLimitDer = new Vector3(posicionInicial.x + distanciaPatrulla, posicionInicial.y, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
+       if(estado == "patrulla")
+       {
+         //si limite derecha
+         if(transform.position >= posicionLimitDer.x)
+            {
+              transform.Translate(velocidadPatrulla, 0, 0);  
+
+            }
+            else
+            {
+                transform.Translate(velocidadPatrulla, 0, 0);  
+            }
         
+        transform.Translate(velocidadPatrulla, 0, 0);
+
+       }
+
+
+
+
     }
 
   void OnTriggerEnter2D(Collider2D col)
